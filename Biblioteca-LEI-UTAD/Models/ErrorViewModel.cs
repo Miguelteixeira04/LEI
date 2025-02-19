054a6454f8e27f6a45a9bd48b7df8b99ebcb6194
Miguelteixeira04 <125875762+Miguelteixeira04@users.noreply.github.com>
2025-02-19 22:56:46 +0000

    Delete Biblioteca-LEI-UTAD/Models directory

--- Biblioteca-LEI-UTAD/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Trabalho.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
